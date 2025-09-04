3328559b9129cd4d5ed260b0d007930cb92f1aad
hyojin4168 <huojin4168@naver.com>
2025-09-04 16:19:34 +0900

    BookVO, LibraryVO, UserVO

--- src/main/java/net/koreate/book/vo/BookVO.java
+++ src/main/java/net/koreate/book/vo/BookVO.java
@@ -1,5 +1,20 @@
 package net.koreate.book.vo;
 
+import java.util.Date;
+
 public class BookVO {
+	
+	private int bno; 			  // 도서 번호
+	private String title;	      // 제목	
+	private String author;	      // 저자
+	private String publisher;	  // 발행 기관
+	private Date p_date;	      // 발행 연도
+	private String cover;	      // 표지 이미지 경로
+	private String is_available;  // 대출 가능 여부
+	private String b_status;      // 상태
+	
+	private int bom_no;			  // 번호 PK
+	private String bom_status;	  // 상태	
+	
 
 }
--- src/main/java/net/koreate/user/vo/UserVO.java
+++ src/main/java/net/koreate/user/vo/UserVO.java
@@ -2,4 +2,12 @@ package net.koreate.user.vo;
 
 public class UserVO {
 
+	private int mno;         // 회원정보
+	private String id;       // 아이디
+	private String pw;       // 비밀번호
+	private String name;     // 이름
+	private String phone;    // 전화번호
+	private String email;    // 이메일
+	private String addr;     // 주소
+	private String status;   // 계정 활성화 여부
 }
