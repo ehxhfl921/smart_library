add6dfe762a9b032f87d17cbce7d2fc3d9188a6d
ehxhfl921 <ehxhfl921@gmail.com>
2025-09-05 15:06:11 +0900

    댓글 컨트롤러 설계 완료

--- src/main/java/net/koreate/board/controller/SuggestionController.java
+++ src/main/java/net/koreate/board/controller/SuggestionController.java
@@ -1,12 +1,16 @@
 package net.koreate.board.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import net.koreate.board.vo.BoardVO;
 import net.koreate.common.utils.Criteria;
 
+@Controller
+@RequestMapping("/suggest")
 public class SuggestionController {
 
 	
