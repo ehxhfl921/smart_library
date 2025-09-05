a609af4292e0b2bd0df32e802995ab72dbd35e4e
ehxhfl921 <ehxhfl921@gmail.com>
2025-09-05 19:46:17 +0900

    BookVO 및 NoticeController 수정, BookDAO, LoanDAO, BookService, BookController 설계 완료

--- src/main/java/net/koreate/book/vo/BookVO.java
+++ src/main/java/net/koreate/book/vo/BookVO.java
@@ -2,6 +2,9 @@ package net.koreate.book.vo;
 
 import java.util.Date;
 
+import lombok.Data;
+
+@Data
 public class BookVO {
 	
 	private int bno; 			  // 도서 번호
@@ -11,10 +14,14 @@ public class BookVO {
 	private Date p_date;	      // 발행 연도
 	private String cover;	      // 표지 이미지 경로
 	private String is_available;  // 대출 가능 여부
-	private String b_status;      // 상태
+	private String b_status;      // 상태 (삭제된 도서인지 아닌지)
 	
-	private int bom_no;			  // 번호 PK
-	private String bom_status;	  // 상태	
+	private int loan_no; 		  // 대출 번호
+	private String user_id;		  // 대출자 아이디 
+	private String loan_status;	  // 도서 대출 상태 (대출 중/반납)
+	
+	private int bom_no;			  // 이달의 도서 번호
+	private String bom_status;	  // 이달의 도서 등록 여부
 	
 
 }
