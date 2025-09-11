7861e75e1d4e2146e886553e5de2d54802f3969f
hyojin4168 <huojin4168@naver.com>
2025-09-11 17:51:23 +0900

    Controller 수정 |board =>분리

--- src/main/java/net/koreate/board/controller/NoticeController.java
+++ src/main/java/net/koreate/board/controller/NoticeController.java
@@ -1,74 +1,106 @@
 package net.koreate.board.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.RequiredArgsConstructor;
+import net.koreate.board.service.NoticeService;
 import net.koreate.board.vo.BoardVO;
 import net.koreate.common.utils.Criteria;
 
 @Controller
 @RequestMapping("/notice")
+@RequiredArgsConstructor
 public class NoticeController {
-	
+   
+   private final NoticeService ns;
+   
 
-	/**
-	 * 페이징 처리된 공지 사항 게시글 목록 페이지 요청 처리
-	 * @param cri
-	 * @param model 공지사항 리스트, PageMaker 객체
-	 */
-	@GetMapping("/list")
-	public String noticeList(Criteria cri, Model model) throws Exception{
-		return null;
-	}
-	
-	/**
-	 * 공지 사항 상세 페이지 보기 요청 처리
-	 * @param nno 전달 받은 공지 사항 번호
-	 * @param model 공지 사항 게시글 정보
-	 */
-	@GetMapping("/detail")
-	public String noticeDetail(int nno, Model model) throws Exception{
-		return null;
-	}
-	
-	/**
-	 * 공지 사항 수정 폼 페이지 요청
-	 * 
-	 * @param nno	수정할 공지 사항 게시글 번호
-	 */
-	@GetMapping("/modifyForm")
-	public String goToModifyForm(int nno, Model model) throws Exception{
-		return null;
-	}
+   /**
+    * 페이징 처리된 공지 사항 게시글 목록 페이지 요청 처리
+    * @param cri
+    * @param model 공지사항 리스트, PageMaker 객체
+    */
+   @GetMapping("/list")
+   public String noticeList(Criteria cri, Model model) throws Exception{
+      System.out.println("공지사항 목록 noticeList 요청");
+       
+       String result = (String) model.getAttribute("result");
+       if (result != null) {
+           model.addAttribute("result", result);
+           model.getAttribute("result");
+       }
+       
+       System.out.println("noticeList Criteria : " + cri);
+       
+       // 조회된 공지사항 게시글 목록
+       Map<String, Object> map = ns.list(cri);
+       model.addAllAttributes(map);
+       return "notice/noticeList";
+   }
+   
+   /**
+    * 공지 사항 상세 페이지 보기 요청 처리
+    * @param nno 전달 받은 공지 사항 번호
+    * @param model 공지 사항 게시글 정보
+    */
+   @GetMapping("/detail")
+   public String noticeDetail(int nno, Model model) throws Exception{
+      BoardVO board = ns.getDetail(nno);
+      model.addAttribute("boardVO", board);
+      
+      return "notice/noticeDetail";
+   }
+   
+   /**
+    * 공지 사항 수정 폼 페이지 요청
+    * 
+    * @param nno   수정할 공지 사항 게시글 번호
+    */
+   @GetMapping("/modifyForm")
+   public String goToModifyForm(int nno, Model model) throws Exception{
+      BoardVO board = ns.getDetail(nno);
+      model.addAttribute(board);
+      return "notice/noticeUpdate";
+   }
 
-	/**
-	 * 공지 사항 등록 요청 처리
-	 * @param vo 등록할 공지 사항 게시글 정보
-	 */
-	@PostMapping("/register")
-	public String noticeRegister(BoardVO vo, Model model) throws Exception{
-		return null;
-	}
-	
-	/**
-	 * 공지 사항 수정 요청 처리
-	 * @param vo 수정할 공지 사항 게시글 정보
-	 */
-	@PostMapping("/modify")
-	public String noticeModify(BoardVO vo, Model model) throws Exception{
-		return null;
-	}
-	
-	/**
-	 * 공지 사항 삭제 요청 처리
-	 * @param nno 삭제 처리할 공지 사항 게시글 번호
-	 */
-	@GetMapping("/delete")
-	public String noticeDelete(int nno, Model model) throws Exception{
-		return null;
-	}
-	
+   /**
+    * 공지 사항 등록 요청 처리
+    * @param vo 등록할 공지 사항 게시글 정보
+    */
+   @PostMapping("/register")
+   public String noticeRegister(BoardVO vo, Model model) throws Exception{
+      System.out.println("param data : " + vo);
+      String result = ns.write (vo);
+      model.addAttribute("msg", result);
+      return "redirect:/";
+   }
+   
+   /**
+    * 공지 사항 수정 요청 처리
+    * @param vo 수정할 공지 사항 게시글 정보
+    */
+   @PostMapping("/modify")
+   public String noticeModify(BoardVO vo, Model model) throws Exception{
+      String result = ns.update(vo);
+      model.addAttribute("result", result);
+      return "redirect:/notice/detail?nno=" + vo.getNno();
+   }
+   
+   /**
+    * 공지 사항 삭제 요청 처리
+    * @param nno 삭제 처리할 공지 사항 게시글 번호
+    */
+   @GetMapping("/delete")
+   public String noticeDelete(int nno, Model model) throws Exception{
+      String result = ns.delete(nno);
+      model.addAttribute("result", result);
+      return "redirect:/notice/list";
+   }
+   
 }
---
+++ src/main/webapp/WEB-INF/views/notice/noticeList.jsp
@@ -0,0 +1,25 @@
+<!-- 공지 사항 목록 페이지 -->
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="../common/header.jsp" %>
+
+<section class="hyojin">
+	<main class="content">
+      <div class="main-header">
+        <h2>공지사항</h2>
+        <button class="write-btn">작성</button>
+      </div>
+
+      <div class="notice-header">
+        <span>No.</span>
+        <span>제목</span>
+        <span>작성자</span>
+        <span>작성일</span>
+      </div>
+      <div class="notice-list">
+      </div>
+    </main>
+</section>
+
+<%@ include file="../common/footer.jsp" %>
