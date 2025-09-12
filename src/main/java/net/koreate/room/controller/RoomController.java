43ca41670085e3d3f5a9e9608858542843995668
rlawlswn1 <kpearl0416@gmail.com>
2025-09-12 20:01:37 +0900

    RoomController 구현 중

--- src/main/java/net/koreate/room/controller/RoomController.java
+++ src/main/java/net/koreate/room/controller/RoomController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,19 +15,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import lombok.RequiredArgsConstructor;
 import net.koreate.common.utils.Criteria;
+import net.koreate.room.service.RoomService;
 
 @Controller
 @RequestMapping("/studyroom")
+@RequiredArgsConstructor
 public class RoomController {
 
+	private final RoomService rs;
+	
 	/**
 	 * 스터디룸 예약 페이지로 이동 요청 처리
 	 */
 	@GetMapping("")
 	public String goToStudyroomPage() {
-		return null;
+		return "room/reservationList";
 	}
 	
 	/**
@@ -81,9 +88,17 @@ public class RoomController {
 	 */
 	@GetMapping("/admin/reservationList")
 	public String goToManageReservation(
-			Criteria cri
+			Criteria cri,Model model
 			) throws Exception{
-		return null;
+		
+		try {
+			Map<String, Object> result = rs.getReservationList(cri);
+	        model.addAttribute("list", result.get("list"));
+	        model.addAttribute("pm", result.get("pm"));
+		}catch(Exception e){  
+	        e.printStackTrace();
+		}	
+			return "admin/manageReservation";
 	}
 	
 	/**
@@ -93,10 +108,14 @@ public class RoomController {
 	 */
 	@PatchMapping("/approve/{rno}")
 	@ResponseBody
-	public ResponseEntity<String> approveReservation(
-			@PathVariable("rno") int rno
+	public String approveReservation(
+			@PathVariable("rno") int rno,
+			Criteria cri,
+			RedirectAttributes rttr
 			) throws Exception{
-		return null;
+		rs.approveReservation(rno);
+		rttr.addFlashAttribute("msg","예약 승인되었습니다.");
+		return "redirect:/studyroom/admin/reservationList?page=" + cri.getPage();
 	}
 	
 	/**
@@ -106,10 +125,14 @@ public class RoomController {
 	 */
 	@PatchMapping("/reject/{rno}")
 	@ResponseBody
-	public ResponseEntity<String> rejectReservation(
-			@PathVariable("rno") int rno
+	public  String rejectReservation(
+			@PathVariable("rno") int rno,
+			Criteria cri,
+			RedirectAttributes rttr
 			) throws Exception{
-		return null;
+		rs.rejectReservation(rno);
+		rttr.addFlashAttribute("msg", "예약 거절되었습니다.");
+		return "redirect:/studyroom/admin/reservationList?page=" + cri.getPage();
 	}
 	
 	/**
@@ -119,10 +142,20 @@ public class RoomController {
 	 */
 	@PatchMapping("/cancel/{rno}")
 	@ResponseBody
-	public ResponseEntity<String> cancelReservation(
-			@PathVariable("rno") int rno
+	public String cancelReservation(
+			@PathVariable("rno") int rno, 
+			Criteria cri,
+			RedirectAttributes rttr
 			) throws Exception{
-		return null;
+		try {
+			rs.cancelReservation(rno);
+			rttr.addFlashAttribute("msg", "예약 취소되었습니다.");
+		}catch(Exception e) {
+			e.printStackTrace();
+			rttr.addFlashAttribute("msg", "취소 처리 중 오류가 발생했습니다. ");
+			
+		}
+		return "redirect:/studyroom/admin/reservationList?page=" + cri.getPage();
 	}
 	
 }
